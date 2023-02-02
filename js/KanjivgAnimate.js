d728b075c153740280bc3cc5834c3e966ff7cacb
Ben Bullock <benkasminbullock@gmail.com>
2023-02-02 13:21:00 +0900

    Various updates to viewer code including animation

---
+++ js/KanjivgAnimate.js
@@ -0,0 +1,163 @@
+'use strict';
+
+var KanjivgAnimate = (function () {
+
+    function KanjivgAnimate(trigger, time) {
+        time = typeof time !== 'undefined' ? time : 500;
+
+        this.setOnClick(trigger, time);
+    }
+
+    /**
+     * Add onclick function to triggers.
+     *
+     * @param {element} trigger
+     * @param {int} time 
+     */
+    KanjivgAnimate.prototype.setOnClick = function setOnClick(trigger, time) {
+        var triggers = document.querySelectorAll(trigger);
+
+        var length = triggers.length;
+
+        var kvganimate = this;
+
+        for (var i = 0; i < length; i++) {
+            triggers[i].onclick = function() {
+                var animate = new KVGAnimator(time);
+
+                animate.play(this);
+
+                return false;
+            };
+        }
+    };
+
+    return KanjivgAnimate;
+})();
+
+var KVGAnimator = (function () {
+
+    function KVGAnimator(time) {
+        this.time = time;
+    }
+
+    /**
+     * Initiate the animation.
+     * 
+     * @param  {element} trigger 
+     */
+    KVGAnimator.prototype.play = function play(trigger) {
+        var svg = this.findTarget(trigger);
+
+        if (!svg || svg.tagName !== 'svg' || svg.querySelectorAll('path') <= 0) {
+            return;
+        }
+
+        this.paths = svg.querySelectorAll('path');
+
+        this.numbers = svg.querySelectorAll('text');
+
+        this.pathCount = this.paths.length;
+
+        this.hideAll();
+
+        this.count = 0;
+
+        var path = this.paths[this.count];
+
+        var number = this.numbers[this.count];
+
+        this.animatePath(path, number);
+    };
+
+    /**
+     * Find data target, if available.
+     * 
+     * @param  {element} trigger
+     */
+    KVGAnimator.prototype.findTarget = function findTarget(trigger) {
+        var attribute = 'data-kanjivg-target';
+
+        if (!trigger.hasAttribute(attribute)) {
+            return trigger;
+        }
+
+        var target = trigger.getAttribute(attribute); 
+
+        return document.querySelector(target);
+    };
+
+    /**
+     * Hide paths and numbers before animation.
+     */
+    KVGAnimator.prototype.hideAll = function hideAll() {
+        for (var i = 0; i < this.pathCount; i++) {
+            this.paths[i].style.display = 'none';
+
+            if (typeof this.numbers[i] !== 'undefined') {
+                this.numbers[i].style.display = 'none';
+            }
+        }  
+    };
+
+    /**
+     * Prepare for animation and call animation function.
+     * 
+     * @param  {element} path
+     * @param  {element} number
+     */
+    KVGAnimator.prototype.animatePath = function animatePath(path, number) {
+        this.length = path.getTotalLength();
+
+        path.style.display = 'block';
+
+        if (typeof number !== 'undefined') {
+            number.style.display = 'block';
+        }
+
+        path.style.transition = path.style.WebkitTransition = 'none';
+
+        path.style.strokeDasharray = this.length + ' ' + this.length;
+
+        path.style.strokeDashoffset = this.length;
+
+        path.getBoundingClientRect();
+
+        this.interval = this.time / this.length;
+
+        this.doAnimation(path);
+    };
+
+    /**
+     * Do the animation.
+     * 
+     * @param  {path} path
+     */
+    KVGAnimator.prototype.doAnimation = function doAnimation(path) {
+            var animator = setTimeout(function () {
+                this.doAnimation(path);
+            }.bind(this, path), this.interval);
+
+            path.style.strokeDashoffset = this.length;
+
+            this.length--;
+
+            if (this.length < 0) {
+                clearInterval(animator);
+
+                this.count += 1;
+
+                if (this.count < this.pathCount) {
+                    var newPath  = this.paths[this.count];
+
+                    var newNumber = this.numbers[this.count];
+
+                    this.animatePath(newPath, newNumber);
+                }
+            }
+    };
+
+    return KVGAnimator;
+})();
+
+//module.exports = KanjivgAnimate;
--- js/kanjiviewer.js
+++ js/kanjiviewer.js
@@ -8,27 +8,34 @@ function kanjiURL(kanji) {
 	return 'kanjivg/kanji/0' + kanji.charCodeAt(0).toString(16) + '.svg'
 }
 
-function loadKanjiVG(img, el, kanji) {
+const kanjiSVGID = "kanji-svg";
+
+// 
+function loadKanjiVG(el, kanji) {
+	var img = document.getElementById("kanji-image");
 	img.innerHTML = '';
 	img.appendChild(el.documentElement);
+	var svg = img.lastChild;
+	svg.id = kanjiSVGID;
 	var hk = kanjiToHex(kanji);
 	var baseID = "kvg:" + hk;
-	console.log(baseID);
 	var baseEl = document.getElementById(baseID);
-	console.log(baseEl);
 	baseEl.style.stroke = "#FC8";
     const paths = baseEl.getElementsByTagName("path");
-	console.log(paths);
 	textBaseID = "kvg:StrokeNumbers_" + hk;
 	var textBaseEl = document.getElementById(textBaseID);
 	const texts = textBaseEl.getElementsByTagName("text");
-	console.log(texts);
+	var displayOrdersEl = document.getElementById("displayOrders");
+	var doc = displayOrdersEl.checked;
 	var i = 0;
 	for (var path of paths) {
 		var colour = "#" + randomColour();
 		path.style.stroke = colour;
-		console.log(" path = "+path + " colour = " + colour);
-		texts[i].style.fill = colour;
+		if (doc) {
+			texts[i].style.fill = colour;
+		} else {
+			texts[i].style.fill = "none";
+		}
 		i++;
 	}
 }
@@ -36,8 +43,8 @@ function loadKanjiVG(img, el, kanji) {
 function randomColour() {
 	var colour = new String();
 	for (var i = 0; i < 3; i++) {
-		var random16 = Math.floor (Math.random() * 16);
-		colour = colour.concat (random16.toString(16).toUpperCase());
+		var random16 = Math.floor(Math.random() * 16);
+		colour = colour.concat(random16.toString(16).toUpperCase());
 	}
 	return colour;
 }
@@ -52,32 +59,24 @@ function getKanjiVG(kanji) {
 	xhr.overrideMimeType("image/svg+xml");
 	xhr.onload = function(e) {
 		// You might also want to check for xhr.readyState/xhr.status here
-		loadKanjiVG(img, xhr.responseXML, kanji);
+		loadKanjiVG(xhr.responseXML, kanji);
 	};
 	xhr.send("");
 }
 
 function setKanjiDrawing(kanji) {
-	console.log("kanji is "+kanji);
-	img = document.getElementById("kanji-image");
-	console.log(img);
 	getKanjiVG(kanji);
 }
+
 KanjiViewer = {
     initialize:function (divName, strokeWidth, fontSize, zoomFactor, displayOrders, colorGroups, kanji) {
-		this.img = document.getElementById("kanji-image");
-        this.paper = new Raphael(divName, '100%', '100%');
-        this.strokeWidth = strokeWidth;
-        this.fontSize = fontSize;
-        this.displayOrders = displayOrders;
-        this.colorGroups = colorGroups;
         this.kanji = kanji;
         this.fetchNeeded = true;
-        this.setZoom(zoomFactor);
         this.refreshKanji();
+		this.animate = new KanjivgAnimate("#animate");
     },
     setZoom:function (zoomFactor) {
-        this.paper.setViewBox(0, 0, 109, 109);
+        this.zoom = zoomFactor;
     },
     setStrokeWidth:function (strokeWidth) {
         this.strokeWidth = strokeWidth;
@@ -100,151 +99,8 @@ KanjiViewer = {
     refreshKanji:function () {
         if (this.fetchNeeded && this.kanji != "") {
 			setKanjiDrawing(this.kanji);
-/*
-			var kanji = this.kanji;
-			var url = kanjiURL(this.kanji);
-            var parent = this;
-            this.paper.clear();
-            var loader = this.paper.text(0, 0, 'Loading ' + this.kanji);
-            loader.attr({
-                'x':50,
-                'y':50,
-                'fill':'black',
-                'font-size':18,
-                'text-anchor':'start'
-            });
-            jQuery.ajax({
-                url:url,
-                dataType:'xml',
-                success:function (results) {
-                    parent.fetchNeeded = false;
-                    parent.xml = results;
-                    parent.drawKanji();
-					setKanjiDrawing(kanji);
-                },
-                statusCode:{
-                    404:function () {
-                        parent.paper.clear();
-                        var error = parent.paper.text(0, 0, parent.kanji + ' not found');
-                        error.attr({
-                            'x':50,
-                            'y':50,
-                            'fill':'black',
-                            'font-size':18,
-                            'text-anchor':'start'
-                        });
-                    }
-                }
-            });
-        } else {
-            this.drawKanji();
-*/
         }
     },
-/*
-    createStroke:function (path, color) {
-        var stroke = this.paper.path(jQuery(path).attr('d'));
-        stroke['initialColor'] = color;
-        stroke.attr({
-            'stroke':color,
-            'stroke-width':this.strokeWidth,
-            'stroke-linecap':'round',
-            'stroke-linejoin':'round'
-        });
-        return stroke;
-    },
-    createHover:function (stroke) {
-        var onEnteredAnim = Raphael.animation({stroke:'black'}, 300);
-        var onLeftAnim = Raphael.animation({stroke:stroke['initialColor']}, 300);
-        stroke.hover(
-                function () {
-                    this.animate(onEnteredAnim);
-                }, function () {
-                    this.animate(onLeftAnim);
-                }
-        );
-    },
-    createHovers:function (strokes) {
-        var parent = this;
-        var onEnteredAnim = Raphael.animation({stroke:'black'}, 300);
-        var onLeftAnim = Raphael.animation({stroke:strokes[0]['initialColor']}, 300);
-        for (var i = 0; i < strokes.length; i++) {
-            var stroke = strokes[i];
-            stroke.hover(
-                    function () {
-                        for (var j = 0; j < strokes.length; j++) {
-                            stroke = strokes[j];
-                            stroke.attr({
-                                'cursor':'pointer'
-                            });
-                            stroke.animate(onEnteredAnim);
-                        }
-                    },
-                    function () {
-                        for (var j = 0; j < strokes.length; j++) {
-                            stroke = strokes[j];
-                            stroke.animate(onLeftAnim);
-                        }
-                    }
-            );
-            stroke.click(function () {
-                parent.setKanji(strokes['element']);
-                parent.refreshKanji();
-            });
-        }
-    },
-    drawKanji:function () {
-        var parent = this;
-        this.paper.clear();
-        Raphael.getColor.reset();
-        var groups = jQuery(this.xml).find('svg > g > g > g');
-        if (!this.colorGroups || groups.length == 0) {
-            jQuery(this.xml).find('path').each(function () {
-                var color = Raphael.getColor();
-                var stroke = parent.createStroke(this, color);
-                parent.createHover(stroke);
-            });
-        } else {
-            groups.each(function () {
-                var color = Raphael.getColor();
-                parent.paper.setStart();
-                jQuery(this).find('path').each(function () {
-                    parent.createStroke(this, color);
-                });
-                var set = parent.paper.setFinish();
-                var element = jQuery(this).attr('kvg:element');
-                if (element == undefined) {
-                    var inners = jQuery(this).find('g');
-                    for (var i = 0; i < inners.length; i++) {
-                        element = jQuery(inners[i]).attr('kvg:element');
-                        if (element !== undefined) {
-                            set['element'] = element;
-                            break;
-                        }
-                    }
-                } else {
-                    set['element'] = element;
-                }
-                parent.createHovers(set);
-            });
-        }
-        jQuery(this.xml).find('text').each(function () {
-            var color = Raphael.color('#000');
-            var text = jQuery(this).text();
-            var transform = jQuery(this).attr('transform');
-            var x = transform.split(' ')[4];
-            var y = transform.split(' ')[5].replace(')', '');
-            var order = parent.paper.text(x, y, text);
-            order.attr({
-                'fill':color,
-                'font-size':parent.fontSize,
-            });
-            if (!parent.displayOrders) {
-                order.hide();
-            }
-        });
-    }
-*/
 };
 function changeBox() {
 	runKanjiViewer();
