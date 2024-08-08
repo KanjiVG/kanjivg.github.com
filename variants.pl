8a5cae3ebacd6d4d4bca4d411a1d2d85be12f2fc
Ben Bullock <benkasminbullock@gmail.com>
2024-08-08 11:41:14 +0900

    Add a simple variant comparision script

---
+++ variants.pl
@@ -0,0 +1,31 @@
+#!/home/ben/software/install/bin/perl
+use Z;
+my $verbose;
+my $infile = <$Bin/variants.html>;
+my $html = read_text ($infile);
+my $actual = `/home/ben/projects/kvgcheck/variants.pl`;
+my %actual;
+my @lines = split (/\s*\n\s*/, $actual);
+for (@lines) {
+    if ($verbose) {
+	print "$_\n";
+    }
+    $actual{$_} = 1;
+}
+my %has;
+while ($html =~ m!<dt>(.*)</dt>!g) {
+    my $v = $1;
+    if ($verbose) {
+	print "$v\n";
+    }
+    $has{$v} = 1;
+    if (! $actual{$v}) {
+	print "$v not in actual list\n";
+    }
+}
+for my $k (keys %actual) {
+    if (! $has{$k}) {
+	print "$k is not in the list yet\n";
+    }
+}
+
